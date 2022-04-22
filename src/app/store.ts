2513df64e66753c296b2e1518a4a5f8732c7adaa
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-04-22 14:27:04 -0500

    add redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { contactsApi } from "../services/api";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [contactsApi.reducerPath]: contactsApi.reducer
   },
-});
-
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(contactsApi.middleware)
+})
---
+++ src/services/api.tsx
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Contact } from '../models/contact.model'
+
+
+export const contactsApi = createApi({
+    reducerPath:'contactsApi',
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3006/" }),
+    endpoints: (builder) => ({
+        contacts: builder.query<Contact[], void>({
+            query: () => '/contacts'
+        })
+    })
+})
